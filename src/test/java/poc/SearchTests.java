0dab5aa7258678edb19dba38be2c031162d176f2
Esther Hinrichsen <estherhinrichsen@Esthers-MacBook-Air.local>
2018-03-26 22:20:17 -0300

    code

--- src/test/java/poc/SearchTests.java
+++ src/test/java/poc/SearchTests.java
@@ -2,77 +2,85 @@ package poc;
 
 import static org.junit.Assert.assertTrue;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class SearchTests {
 	
-	WebDriver browser;
-	String url;
 	
+	String url;
+	RemoteWebDriver driver;
 	@Before
 	public void readParameters() {
-		String pBrowser = System.getProperty("browser");
-		if(pBrowser.equals("firefox")) {
-			 browser = new FirefoxDriver();
-		} else if (pBrowser.equals("chrome")) {
-			browser = new ChromeDriver();
+		
+		DesiredCapabilities dc;
+		
+		String browser = System.getProperty("browser");
+		if(browser.equals("firefox")) {
+			 dc = new DesiredCapabilities().firefox();
+		} else if (browser.equals("chrome")) {
+			dc = new DesiredCapabilities().chrome();
 		} else {
-			browser = new SafariDriver();
+			dc = new DesiredCapabilities().internetExplorer();
 		}	
 		
 		url = System.getProperty("url");
-		 
+		
+		try {
+			driver = new RemoteWebDriver(new URL("http://10.11.12.13:4444/wd/hub"), dc);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	@Test
 	public void searchTest() {
-		browser.get(url);
-		WebElement campoPesquisa = browser.findElement(By.name("q"));
+		driver.get(url);
+		WebElement campoPesquisa = driver.findElement(By.name("q"));
 		campoPesquisa.sendKeys("Teste");
-		WebElement botaoPesquisar = browser.findElement(By.name("btnK"));
+		WebElement botaoPesquisar = driver.findElement(By.name("btnK"));
 		botaoPesquisar.click();
-		WebElement speedTestLink = browser.findElement(By.partialLinkText("www.speedtest.net/pt"));
+		WebElement speedTestLink = driver.findElement(By.partialLinkText("www.speedtest.net/pt"));
 		assertTrue(speedTestLink.isDisplayed());
 
 	}
 	@Test
 	public void searchItau() {
 		
-		browser.get(url);
-		WebElement campoPesquisa = browser.findElement(By.name("q"));
+		driver.get(url);
+		WebElement campoPesquisa = driver.findElement(By.name("q"));
 		campoPesquisa.sendKeys("itau");
-		WebElement botaoPesquisar = browser.findElement(By.name("btnK"));
+		WebElement botaoPesquisar = driver.findElement(By.name("btnK"));
 		botaoPesquisar.click();
-		WebElement speedTestLink = browser.findElement(By.partialLinkText("www.itau.com.br/"));
+		WebElement speedTestLink = driver.findElement(By.partialLinkText("www.itau.com.br/"));
 		assertTrue(speedTestLink.isDisplayed());
 	
 	}
 	
 	@Test
 	public void searchRede() {
-		WebDriver browser = new FirefoxDriver();
-		
-		browser.get(url);
-		WebElement campoPesquisa = browser.findElement(By.name("q"));
+		driver.get(url);
+		WebElement campoPesquisa = driver.findElement(By.name("q"));
 		campoPesquisa.sendKeys("rede");
-		WebElement botaoPesquisar = browser.findElement(By.name("btnK"));
+		WebElement botaoPesquisar = driver.findElement(By.name("btnK"));
 		botaoPesquisar.click();
-		WebElement speedTestLink = browser.findElement(By.partialLinkText("www.userede.com.br/"));
+		WebElement speedTestLink = driver.findElement(By.partialLinkText("www.userede.com.br/"));
 		assertTrue(speedTestLink.isDisplayed());
 
 	}
 	
 	@After
 	public void closeBrowser() {
-		browser.close();
+		driver.close();
 	}
 
 }
